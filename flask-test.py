c9e4df47f4b1ac2cb3d273287e153c8b575b87b1
noazlee <noahzlee5@gmail.com>
2024-03-27 13:15:03 -0500

    added logic for new game and next move

--- flask-test.py
+++ flask-test.py
@@ -2,16 +2,55 @@
 
 import flask
 import json
+import random
 
 app = flask.Flask(__name__)
+num = 0
 
-@app.route('/do-stuff/<input>')
-def my_function(input):
-    the_dictionary = {}
-    the_dictionary['name']='Matt'
-    the_dictionary['stuff']=input
-
+#new game
+@app.route('/newgame/<player>')
+def newGame(num):
+    num += 1
+    the_dictionary = {'ID': num} # ID: num 
     return json.dumps(the_dictionary)
+
+# Read in current game state
+# Decide what move to make - random number and then dont do illegal move
+# Return game state
+
+#existing game
+@app.route('/nextmove/<gameID>/<oppCol>/<state>')
+def nextMove(gameID, oppCol, state):
+    
+    s = state
+    randNum = random.randint(0,6)
+    success = False
+    currentPos = randNum
+
+    numIterations = 0
+
+    player = s.split('#')[0]
+    board = s.split('#')[1]
+
+    while not success or numIterations < 7:
+        if board[currentPos] != "-":
+            currentPos += 7
+            if currentPos > 42:
+                numIterations += 1
+                randNum += 1
+                currentPos = randNum 
+                if currentPos > 6:
+                    currentPos = 0
+        else:
+            success = True
+            board[currentPos] = player
+
+    if success == False:
+        print("No legal moves available")
+
+    return json.dumps({'ID': gameID, 'Col': randNum, 'State': board})
+
+
 
 if __name__ == '__main__':
     host = 'localhost'
