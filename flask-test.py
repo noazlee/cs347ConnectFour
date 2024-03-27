2f805d0ecd60effe93229fe65027353ce0cc4b16
Warren Kozak <warren.john.kozak@gmail.com>
2024-03-27 14:20:05 -0400

    Change port info

--- flask-test.py
+++ flask-test.py
@@ -53,6 +53,6 @@ def nextMove(gameID, oppCol, state):
 
 
 if __name__ == '__main__':
-    host = 'localhost'
-    port = 5555
+    host = '0.0.0.0'
+    port = 5127
     app.run(host=host, port=port, debug=True)
