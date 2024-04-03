fd270038d911b1a9897b87aac73456c2c529366f
noazlee <noahzlee5@gmail.com>
2024-04-03 13:16:45 -0500

    edited

--- MattCode/main/connect4-server.py
+++ MattCode/main/connect4-server.py
@@ -11,8 +11,8 @@ import urllib
 
 # IMPORTANT: Change these Host Names and Ports as needed
 # This should be the Ports (and host names) where the AI players are listening
-ai_one_url = "http://aione:1234"
-ai_two_url = "http://aitwo:1234"
+ai_one_url = "http://ai1:5000"
+ai_two_url = "http://ai2:5001"
 
 max_gamenum = 1
 state_dict = {}
@@ -24,6 +24,8 @@ ai_url_lock = threading.Lock()
 app = Flask(__name__)
 
 # This function returns the empty board state for the start of the game
+
+
 def blank_state():
     state = "X#"
     for i in range(42):
@@ -33,12 +35,16 @@ def blank_state():
 # Takes a Board which is a string of 42 characters
 #  Interprets this Board as a grid of 6 rows and 7 columns
 #  Returns the character in a particular row and column
+
+
 def charAt(board, row, col):
     index = col + row*7
     return board[index]
 
 # Checks for a vertical win where the top-most piece is at row,col
-def isWinDown(board,row, col):
+
+
+def isWinDown(board, row, col):
     if row > 2:
         return False
     if charAt(board, row, col) != '-':
@@ -49,7 +55,9 @@ def isWinDown(board,row, col):
     return False
 
 # Checks for a Diagonal win where the Top Left Piece in the Win is at row,col
-def isWinDownRight(board,row, col):
+
+
+def isWinDownRight(board, row, col):
     if row > 2:
         return False
     if col > 3:
@@ -62,7 +70,9 @@ def isWinDownRight(board,row, col):
     return False
 
 # Checks for a Diagonal win where the Top Right Piece in the Win is at row,col
-def isWinDownLeft(board,row, col):
+
+
+def isWinDownLeft(board, row, col):
     if row > 2:
         return False
     if col < 3:
@@ -75,7 +85,9 @@ def isWinDownLeft(board,row, col):
     return False
 
 # Checks for a Horizontal win where the Left-most piece is at row, col
-def isWinRight(board,row, col):
+
+
+def isWinRight(board, row, col):
     if col > 3:
         return False
     if charAt(board, row, col) != '-':
@@ -97,19 +109,21 @@ def isGameOver(state):
 
     for row in range(6):
         for col in range(7):
-            if isWinDown(board,row,col):
+            if isWinDown(board, row, col):
                 return True
-            if isWinRight(board,row,col):
+            if isWinRight(board, row, col):
                 return True
-            if isWinDownRight(board,row,col):
+            if isWinDownRight(board, row, col):
                 return True
-            if isWinDownLeft(board,row,col):
+            if isWinDownLeft(board, row, col):
                 return True
-    
+
     return False
 
 # This function is intended to be used inside a thread
 #  ... This function plays through a game of Connect 4 between two AI players
+
+
 def play_game(num):
     global state_dict
     global ai_url_dict
@@ -123,7 +137,7 @@ def play_game(num):
     ids['X'] = str(ai_answer['ID'])
     ai_answer = requests.get(ai_urls['O'] + '/newgame/O').json()
     ids['O'] = str(ai_answer['ID'])
-    
+
     cur_state = blank_state()
     state_lock.acquire()
     state_dict[num] = cur_state
@@ -137,43 +151,45 @@ def play_game(num):
     # Repeatedly Ask the AI players for their moves
     while not isGameOver(cur_state):
 
-        #The hash symbol in the state needs to be replaced by the code %23
+        # The hash symbol in the state needs to be replaced by the code %23
         url_state = urllib.parse.quote(cur_state)
-        query_url = ai_urls[player] + '/nextmove/' + ids[player] + '/' + col + '/' + url_state
+        query_url = ai_urls[player] + '/nextmove/' + \
+            ids[player] + '/' + col + '/' + url_state
 
-        #Debug write to stderr to see output in the terminal window
-        #sys.stderr.write(query_url)
-        
+        # Debug write to stderr to see output in the terminal window
+        # sys.stderr.write(query_url)
+
         ai_answer = requests.get(query_url).json()
         cur_state = ai_answer['state']
-        col = str( ai_answer['col'] )
+        col = str(ai_answer['col'])
 
         if player == 'X':
             player = 'O'
         else:
             player = 'X'
-        
+
         state_lock.acquire()
         state_dict[num] = cur_state
         state_lock.release()
 
-        num_seconds = random.randint(1,4)
+        num_seconds = random.randint(1, 4)
         time.sleep(num_seconds)
 
     return True
+
 
 @app.route('/')
 def homepage():
     return render_template("homepage.html")
 
 
-#Returns the state of the Game with the given Game Number
+# Returns the state of the Game with the given Game Number
 @app.route('/gamestate/<num>')
 def gamestate(num):
     global state_dict
-    
+
     state_lock.acquire()
-    if(num in state_dict):
+    if (num in state_dict):
         state = state_dict[num]
     else:
         state = blank_state()
@@ -186,31 +202,31 @@ def gamestate(num):
     return json.dumps(my_dict)
 
 
-#Starts a game between the two AI players
+# Starts a game between the two AI players
 @app.route('/watchgame')
 def watchgame():
 
-    #Select an unused game number
+    # Select an unused game number
     global max_gamenum
     game_number = max_gamenum
     max_gamenum += 1
 
     players = {}
-    #Randomly decide which AI is X and which is O
-    if random.randint(0,1) == 0:
+    # Randomly decide which AI is X and which is O
+    if random.randint(0, 1) == 0:
         players['X'] = ai_one_url
         players['O'] = ai_two_url
     else:
         players['O'] = ai_one_url
         players['X'] = ai_two_url
     ai_url_dict[str(game_number)] = players
-    
-    #Create a new thread to run the game
+
+    # Create a new thread to run the game
     start_new_thread(play_game, (str(game_number), ))
-    
+
     return render_template("watchgame.html", GameNum=game_number)
 
 
 if __name__ == '__main__':
     my_port = 5000
-    app.run(host='0.0.0.0', port = my_port) 
+    app.run(host='0.0.0.0', port=my_port)
